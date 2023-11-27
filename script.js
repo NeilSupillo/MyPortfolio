9b6fc69ae8a292aa8fcbc51429716a874021345f
Neil Supillo <neilsupillo@gmail.com>
2023-11-27 15:05:25 +0800

    src ./

--- script.js
+++ script.js
@@ -107,8 +107,8 @@ let icon = document.querySelector(".mode");
 iconDiv.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "/assets/sun.png";
+    icon.src = "./assets/sun.png";
   } else {
-    icon.src = "/assets/moon.png";
+    icon.src = "./assets/moon.png";
   }
 };
